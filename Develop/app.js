599fb3f80313e8e6a0d377af5faa3d5116bfa5d8
dianaz <stingray2091@gmail.com>
2020-06-10 23:21:54 -0400

    setting up the user prompts

--- Develop/app.js
+++ Develop/app.js
@@ -9,8 +9,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output"); ``
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 console.log(OUTPUT_DIR);
 console.log(outputPath);
-
-
 const render = require("./lib/htmlRenderer");
 
 
@@ -18,48 +16,69 @@ const render = require("./lib/htmlRenderer");
 // and to create objects for each team member (using the correct classes as blueprints!)
 const questions = [
     {
-        type: "input",// multiple selection?? 
-        name: "Type of Employee",
-        message: " Please Enter "
-    },
-]
-inquirer
-    .prompt(questions)
-    .then(answers => {
+        type: "input",
+        name: "employeeType",
+        message: " Please Select Employee Type ",
+        choice: ["Manager", "Intern", "Engineer"]
+    }
+];
 
-        console.log(answers);
 
-        const Elias = new Engineer("gitHub")
 
-        //user wants to create one manager and two intern and two engineers. lets assume what you have is an array of objects. the data returned would be name, id, email or different properties classes. once data is provided take it and inside the inquirer code and create one employee from .then create intern objects. 
-
-        // based on user input creat new object. saved in an array once object is saved then creat html. 
-        //1 ask info
-        //create and save file  
-        // hint 
-    })
-    .catch(error => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else when wrong
-        }
-    });
 // creat new instances of classes with new data
 const manager1 = new Manager("Diana", 11, "diana@gaeacoop.org", "D342");
-console.log(manager1);
 const intern1 = new Intern("Diana", 11, "diana@gaeacoop.org", "UNH");
-console.log(manager1);
 const engineer1 = new Engineer("Diana", 11, "diana@gaeacoop.org", "diana603");
-console.log(manager1, intern1, engineer1);
+const employees = [manager1, intern1, engineer1];
+
+//initialize function
+async function initialize() {
+    //ask the questions so we can fill the class objects
+    await inquirer
+        .prompt(questions)
+        .then(async answers => {
+            console.log("int he function");
+        })
+        .catch(error => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else when wrong
+            }
+        });
+
+    //get the renderedHTML (which is just a string of html tags)
+    const renderHTML = await render(employees);
+
+    //check if the output directory exists, if not create it
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+
+    //writes renderedHTML to a file in outputpath
+    fs.writeFile(outputPath, renderHTML, (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("file created");
+        }
+    });
+
+}
+
+//call the initialie function above
+initialize();
+
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-const employees = [manager1, intern1, engineer1];
-const renderHTML = render(employees);
-console.log(renderHTML);
+
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -67,17 +86,7 @@ console.log(renderHTML);
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.  
 // checking if output folder exist if not it will make one.// 
-if (!fs.existsSync(OUTPUT_DIR)) {
-    fs.mkdirSync(OUTPUT_DIR);
-}
-//writes renderedHTML to a file in outputpath
-fs.writeFile(outputPath, renderHTML, (err) => {
-    if (err) {
-        throw err;
-    } else {
-        console.log("file created");
-    }
-});
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
